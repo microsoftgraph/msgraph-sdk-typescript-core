309f649031be38b7910a8de1810ad37f993ae703
Musale Martin <martinmusale@microsoft.com>
2024-02-14 15:01:29 +0300

    Move all the files from the root folder to the src folder

--- index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./baseGraphRequestAdapter";
-export * from "./graphHttpClient";
-export * from "./middleware";
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./adapter";
+export * from "./http";
+export * from "./middleware";
+export * from "./utils/constants";
+export * from "./utils/version";
