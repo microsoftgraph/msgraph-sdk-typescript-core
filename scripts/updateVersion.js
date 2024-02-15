a0f2a2df65f688662730fcb1e346e263409240ca
Martin Musale <martinmusale@microsoft.com>
2024-02-15 17:27:19 +0300

    Fix failing prettier check on Version file generation

--- scripts/updateVersion.js
+++ scripts/updateVersion.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const project = require("../package.json");
 const { version } = project;
-const coreVersionString = `export const coreVersion = "[VERSION]"`;
+const coreVersionString = `export const coreVersion = "[VERSION]";\n`;
 const writeVersionToFile = (path) => {
   const data = coreVersionString.replace("[VERSION]", version);
   console.log(`Updating the version to v${version}`);
