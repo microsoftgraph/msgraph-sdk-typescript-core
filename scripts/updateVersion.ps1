3088257814c9793df488b10ccabf58471af235a9
Musale Martin <martinmshale@gmail.com>
2024-02-15 11:42:55 +0300

    Update constantsFilePath in updateVersion script.
    Co-authored-by: Vincent Biret <vincentbiret@hotmail.com>

--- scripts/updateVersion.ps1
+++ scripts/updateVersion.ps1
@@ -2,5 +2,5 @@ $packageJsonFilePath = Join-Path $PSScriptRoot "../package.json"
 $packageJson = Get-Content $packageJsonFilePath -Raw | ConvertFrom-Json
 $version = $packageJson.version
 Write-Information "Updating version to $version"
-$constantsFilePath = Join-Path $PSScriptRoot "../utils/version.ts"
+$constantsFilePath = Join-Path $PSScriptRoot "../src/utils/version.ts"
 Write-Output "export const coreVersion = `"$version`";" | Set-Content $constantsFilePath -Verbose
