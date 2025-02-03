2725aace6a5d4ff713e2a8bf213e7e22f985dcde
rkodev <43806892+rkodev@users.noreply.github.com>
2025-02-03 23:34:18 +0300

    feat: adds graph allowed hosts

---
+++ src/authentication/AzureIdentityAccessTokenProvider.ts
@@ -0,0 +1,26 @@
+import { GetTokenOptions, TokenCredential } from "@azure/core-auth";
+import { AzureIdentityAccessTokenProvider } from "@microsoft/kiota-authentication-azure";
+import { ObservabilityOptions } from "@microsoft/kiota-authentication-azure/dist/es/src/observabilityOptions";
+
+export class GraphAzureIdentityAccessTokenProvider extends AzureIdentityAccessTokenProvider {
+  public constructor(
+    credentials: TokenCredential,
+    scopes?: string[],
+    options?: GetTokenOptions | undefined,
+    allowedHosts?: Set<string>,
+    observabilityOptions?: ObservabilityOptions,
+    isCaeEnabled?: boolean,
+  ) {
+    if (!allowedHosts || allowedHosts.size === 0) {
+      allowedHosts = new Set([
+        "graph.microsoft.com",
+        "graph.microsoft.us",
+        "dod-graph.microsoft.us",
+        "graph.microsoft.de",
+        "microsoftgraph.chinacloudapi.cn",
+        "canary.graph.microsoft.com",
+      ]);
+    }
+    super(credentials, scopes, options, allowedHosts, observabilityOptions, isCaeEnabled);
+  }
+}
