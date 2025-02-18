d148db14732b3c09dcc85c0c2647f60c3eaa96a3
rkodev <43806892+rkodev@users.noreply.github.com>
2025-02-18 11:09:10 +0300

    update comment

--- src/content/BatchResponseContent.ts
+++ src/content/BatchResponseContent.ts
@@ -27,7 +27,7 @@ export interface BatchItem {
 /**
  * @interface
  * Signature representing Batch response body
- * @property {KeyValuePairObject[]} responses - An array of key value pair representing response object for every request
+ * @property {BatchResponseBody[]} responses - An array of key value pair representing response object for every request
  * @property {string} [@odata.nextLink] - The nextLink value to get next set of responses in case of asynchronous batch requests
  */
 export interface BatchResponseBody {
