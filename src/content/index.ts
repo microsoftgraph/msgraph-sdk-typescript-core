0c76f081a4af88976d3ee22ac545a617a59b6dfd
rkodev <43806892+rkodev@users.noreply.github.com>
2025-02-28 00:46:11 +0300

    update index file

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,4 +1,6 @@
-export * from "./BatchRequestStep";
+export * from "./BatchRequestBuilder.js";
 export * from "./BatchRequestContent.js";
 export * from "./BatchRequestContentCollection.js";
+export * from "./BatchRequestStep";
 export * from "./BatchResponseContent.js";
+export * from "./BatchResponseContentCollection.js";
